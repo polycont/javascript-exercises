1189d4925da548cc83a68c8d643f7a786cf066a7
Cohen Coberly <22184868+polycont@users.noreply.github.com>
2022-08-05 05:05:14 -0500

    Finish 2nd JS exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let stringVar = '';
+    for (i = 0; i < num; ++i) {
+        stringVar = stringVar.concat(string);
+    }
+    if (num < 0) {
+        return "ERROR";
+    }
+    return stringVar;
+}
 
-};
 
 // Do not edit below this line
 module.exports = repeatString;
