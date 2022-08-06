09114444471a67d4cab380cc225ae43578e380c7
Cohen Coberly <22184868+polycont@users.noreply.github.com>
2022-08-06 04:32:55 -0500

    Finish JS exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,21 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let stringVar = string;
+    arrayVar = stringVar.split("",stringVar.length);
+    arrayLength = arrayVar.length;
+    let newArray = [];
+    let x = 1;
+    console.log(arrayVar);
+
+    for (i = 0; i < arrayLength; ++i) {
+
+        newArray.push(arrayVar[arrayVar.length-x]);
+        x = ++x;
+        console.log(newArray);
+
+    }
+    newArray = newArray.join('');
+    console.log(newArray);
+    return newArray;
 
 };
 
