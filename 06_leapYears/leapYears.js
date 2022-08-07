8e8713792a43c356b3a2149afbcaba08c6248169
Cohen Coberly <22184868+polycont@users.noreply.github.com>
2022-08-07 18:15:59 -0500

    Finish JS exercise 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,16 @@
-const leapYears = function() {
+const leapYears = function(leapNum) {
+
+    if (leapNum % 4 === 0 && leapNum % 100 !== 0) {
+        return true;
+    
+    } else if (leapNum % 400 === 0) {
+        return true;
+        
+    } else 
+    return false;
 
 };
+    
 
 // Do not edit below this line
 module.exports = leapYears;
