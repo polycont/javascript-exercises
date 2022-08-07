a616f332e72413341e74eeb00970a236a8fcf3ca
Cohen Coberly <22184868+polycont@users.noreply.github.com>
2022-08-07 01:34:20 -0500

    Complete JS exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,23 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...arrayVal) {
 
-};
+    // Declare initial variables to gather function input
+    let array1 = array;
+    let array2 = arrayVal;
+
+    // Create empty array to store output
+    let newArray = [];
+
+    for (i = 0; i < 9; ++i) {
+
+        if (array1.includes(array2[i])) {
+            let index = array1.indexOf(array2[i]);
+            array1.splice(index, 1);
+        }
+        console.log(array1);
+}
+    return array1;
+
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
