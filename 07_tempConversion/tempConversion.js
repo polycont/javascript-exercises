ad4aa81e19f1942c43a8aef0c79ab34a2801d77d
Cohen Coberly <22184868+polycont@users.noreply.github.com>
2022-08-07 19:41:14 -0500

    Finish JS exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,14 @@
-const ftoc = function() {
-
+const ftoc = function(fTemp) {
+  let finalTemp = (fTemp + 40) / 1.8 - 40;
+  console.log(finalTemp);
+  finalTemp = (Math.round(finalTemp * 10)) / 10;
+  return finalTemp;
 };
 
-const ctof = function() {
-
+const ctof = function(cTemp) {
+  let finalTemp = (cTemp + 40) * 1.8 - 40;
+  finalTemp = (Math.round(finalTemp * 10)) / 10;
+  return finalTemp;
 };
 
 // Do not edit below this line
