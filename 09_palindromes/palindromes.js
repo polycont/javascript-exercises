05650a9b1f0f442c66728e5a963e157ec73faee3
Cohen Coberly <22184868+polycont@users.noreply.github.com>
2022-08-16 15:42:59 -0500

    Finish palindrome exercise (badly)

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,36 @@
-const palindromes = function () {
+const palindromes = (string) => {
 
+    console.log(string);
+
+    string = string.toLowerCase();
+    string = string.replaceAll(' ', '');
+    string = string.replaceAll(', ', '');
+    string = string.replaceAll(',', '');
+    string = string.replaceAll('.', '');
+    string = string.replaceAll('!', '');
+    console.log(string);
+
+
+    array = string.split('', string.length)
+    let reversed = [];
+    let x = 1
+    for (i = 0; i < array.length; ++i) {
+        reversed.push(array[array.length-x]);
+        x = ++x;
+    }
+    reversed = reversed.join('');
+
+    // Replacing non-letter characters w/o RegEx.
+    string = string.replaceAll(' ', '');
+    string = string.replaceAll(', ', '');
+    string = string.replaceAll(',', '');
+    string = string.replaceAll('.', '');
+    string = string.replaceAll('!', '');
+
+    if (string === reversed) return true;
+    return false;
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;
