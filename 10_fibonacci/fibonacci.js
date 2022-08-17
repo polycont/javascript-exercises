0a05c85ae7c6f2e4c41172d12207091e1dd27bb0
Cohen Coberly <22184868+polycont@users.noreply.github.com>
2022-08-16 19:56:02 -0500

    Finish Fibonacci sequence Js exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,27 @@
-const fibonacci = function() {
+const fibonacci = (fibceil) => {
+    if (fibceil < 0) return 'OOPS';
+    if (typeof fibceil !== 'number') Number(fibceil);
 
+    let array = [];
+    let fibsum = 0;
+
+    array.push(1);
+    array.push(1);
+
+    let a = 0;
+    let b = 1;
+
+    for (i = 0; i < fibceil; ++i) {
+        fibsum = array[a] + array[b];
+        array.push(fibsum);
+        ++a;
+        ++b;
+    }
+
+    console.log(array);
+    return array[fibceil - 1]
 };
+
 
 // Do not edit below this line
 module.exports = fibonacci;
