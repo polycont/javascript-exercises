5d2ea4e92bc068182f59ff27bcf7648c92bc8e37
Cohen Coberly <22184868+polycont@users.noreply.github.com>
2022-08-17 01:41:14 -0500

    Finish findtheoldest js exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,26 @@
-const findTheOldest = function() {
+const findTheOldest = (people) => {
+    const oldest = people.sort((a, b) => {
+        let aYOD = a.yearOfDeath;
+        let aYOB = a.yearOfBirth;
+        let bYOD = b.yearOfDeath;
+        let bYOB = b.yearOfBirth;
+        
+        if (!("yearOfDeath" in a)) {
+            aYOD = new Date().getFullYear();
+            console.log(aYOD);
+        } else if (!("yearOfDeath" in b)) {
+            bYOD = new Date().getFullYear();
+            console.log(bYOD);
+        }
 
+        if ((aYOD - aYOB) > (bYOD - bYOB)) {
+            return 1;
+        } else {
+            return -1;
+        }
+        
+    })
+    return oldest[oldest.length - 1];
 };
 
 // Do not edit below this line
